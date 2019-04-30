34f4746ce773f6987c42313866f63ad431ff5b6f
Jim Kriz <jim@kriz.net>
2019-04-29 22:23:23 -0400

    Updated for new Wunderground API

--- skills/GetCurrentWeather/getCurrentWeather.js
+++ skills/GetCurrentWeather/getCurrentWeather.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let http = require('http');
+let http = require('https');
 let Skill = require('../skill');
 
 class GetCurrentWeather extends Skill {
@@ -17,13 +17,20 @@ class GetCurrentWeather extends Skill {
     // Call wunderground API to get current conditions:
     getText(callback) {
         let apiKey = process.env.WUNDERGROUND_API_KEY;
-        let location = process.env.WUNDERGROUND_PWS_ID;        
+        let location = process.env.WUNDERGROUND_PWS_ID;
 
+        // New API: https://docs.google.com/document/d/1eKCnKXI9xnoMGRRzOL1xPCBihNV2rOet08qpE_gArAY/edit
+        // let options = {
+        //     hostname: 'api.wunderground.com',
+        //     path: `/api/${apiKey}/conditions/q/${location}.json`,
+        //     method: 'GET'
+        // };
+        
         let options = {
-            hostname: 'api.wunderground.com',
-            path: `/api/${apiKey}/conditions/q/${location}.json`,
+            hostname: 'api.weather.com',
+            path: `/v2/pws/observations/current?stationId=${location}&format=json&units=e&apiKey=${apiKey}`,
             method: 'GET'
-        };
+        }
 
         let body = '';
 
@@ -34,9 +41,13 @@ class GetCurrentWeather extends Skill {
 
                 console.log(`Result: ${JSON.stringify(result)}`);
 
-                let currentTemp = Math.round(result.current_observation.temp_f);
-                let feelsLikeTemp = Math.round(result.current_observation.feelslike_f);
-                let responseText = `Right now in ${result.current_observation.display_location.city}, it is ${result.current_observation.weather} and the temperature is ${currentTemp} degrees`;
+                // let currentTemp = Math.round(result.current_observation.temp_f);
+                // let feelsLikeTemp = Math.round(result.current_observation.feelslike_f);
+                // let responseText = `Right now in ${result.current_observation.display_location.city}, it is ${result.current_observation.weather} and the temperature is ${currentTemp} degrees`;
+                let observation = result.observations[0];
+                let currentTemp = Math.round(observation.imperial.temp);
+                let feelsLikeTemp = currentTemp >= 70 ? Math.round(observation.imperial.heatIndex) : Math.round(observation.imperial.windChill);
+                let responseText = `Right now in ${observation.neighborhood}, the temperature is ${currentTemp} degrees`;
                 responseText += (currentTemp === feelsLikeTemp) ? '.' : `, but it feels like ${feelsLikeTemp}.`;
                 callback(responseText);
             });
